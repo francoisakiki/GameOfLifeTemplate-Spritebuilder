2017fc74bf25f9f6c6f684c9a37c473bc40020b0
Francois Akiki <fakiki@Francoiss-MacBook-Air.local>
2014-07-14 12:32:43 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/SpriteBuilder Resources/Grid.m
+++ GameOfLife.spritebuilder/SpriteBuilder Resources/Grid.m
@@ -81,17 +81,5 @@ static const int GRID_COLUMNS = 10;
     //get the row and column that was touched, return the Creature inside the corresponding cell
 }
 
-@interface evolveStep: CCSprite
-
-{
-    //update each Creature's neighbor count
-    [self countNeighbors];
-    
-    //update each Creature's state
-    [self updateCreatures];
-    
-    //update the generation so the label's text will display the correct generation
-    _generation++;
-}
 
 @end
